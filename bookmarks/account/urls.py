33d1c5ed9a740c1206cb863db9c0dc7070ebab5c
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-11-18 21:44:42 +1000

    Add authentication forms

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,5 +9,34 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('images', views.images, name='images'),
-    path('people', views.people, name='people')
+    path('people', views.people, name='people'),
+    path(
+        'password-change', auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password-change-done',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+    path(
+        'password-reset',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset-done',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password-reset-complete',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    )
 ]
