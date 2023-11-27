3be6f3cdbfd98fb03ec8e96db4d4e5f5dd84f3c3
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-11-27 21:30:07 +1000

    Add images app

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'social_django',
-    'django_extensions'
+    'django_extensions',
+    'images.apps.ImagesConfig'
 ]
 
 MIDDLEWARE = [
