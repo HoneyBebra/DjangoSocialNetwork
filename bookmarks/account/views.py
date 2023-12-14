c22c1a52202df93cacdbe5f7c63727c0ac4bd221
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-12-14 22:49:10 +1000

    Added subscribing

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,13 +2,15 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import (
     UserRegistrationForm,
     UserEditForm,
     ProfileEditForm
 )
-from .models import Profile
+from .models import Profile, Contact
 
 
 @login_required
@@ -17,15 +19,6 @@ def dashboard(request):
         request,
         'account/dashboard.html',
         {'section': 'dashboard'}
-    )
-
-
-@login_required
-def people(request):
-    return render(
-        request,
-        'account/people.html',
-        {'section': 'people'}
     )
 
 
@@ -123,3 +116,28 @@ def user_detail(request, username):
             'user': user
         }
     )
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+
+    return JsonResponse({'status': 'error'})
